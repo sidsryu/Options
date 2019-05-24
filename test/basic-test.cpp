e46f991eda2c7ae50c219067c892926979245adc
Sid S. Ryu <sid@sidryu.com>
2019-05-24 21:31:41 +0900

    코드정리
    - 유닛테스트 코드컨벤션 정리
    - 다중 테스트 개별 분리

--- test/basic-test.cpp
+++ test/basic-test.cpp
@@ -23,33 +23,38 @@ TEST_CASE("Parse command line for easy case.", "[basic]")
 	SECTION("Check values")
 	{
 		std::wstring filename;
-		int		count;
+		int	count;
 
 		options.Add(L"file", L"", filename);
 		options.Add(L"count", L"", count);
 
 		REQUIRE(options.Parse(L"-file filename -count 10"));
 
-		CHECK(L"filename" == filename);
+		CHECK(filename == L"filename");
 		CHECK(count == 10);
 	}
 
 	SECTION("Check multiple values")
 	{
-		std::wstring filename;
 		std::vector<std::wstring> fileList;
-
-		options.Add(L"file", L"", filename);
 		options.Add(L"file", L"", fileList);
 
 		REQUIRE(options.Parse(L"-file first second third"));
 
-		CHECK(L"first second third" == filename);
-
 		REQUIRE(3 == fileList.size());
-		CHECK(L"first" == fileList[0]);
-		CHECK(L"second" == fileList[1]);
-		CHECK(L"third" == fileList[2]);
+		CHECK(fileList[0] == L"first");
+		CHECK(fileList[1] == L"second");
+		CHECK(fileList[2] == L"third");
+	}
+
+	SECTION("Check multiple values with a single-capture")
+	{
+		std::wstring filenames;
+		options.Add(L"file", L"", filenames);
+
+		REQUIRE(options.Parse(L"-file first second third"));
+
+		CHECK(filenames == L"first second third");
 	}
 
 	SECTION("Use custom switch")
@@ -83,89 +88,186 @@ TEST_CASE("Parse command line for easy case.", "[basic]")
 		options.Add(L"file", L"", filename);
 		options.SetSeparator(L"=");
 
-		CHECK(options.Parse(L"-file=filename"));
+		REQUIRE(options.Parse(L"-file=filename"));
 
-		CHECK(L"filename" == filename);
+		CHECK(filename == L"filename");
 	}
 
 	SECTION("Use custom value serial separator")
 	{
-		std::wstring filename;
 		std::vector<std::wstring> fileList;
 
 		options.Add(L"file", L"", fileList);
-		options.Add(L"file", L"", filename);
 		options.SetSplitter(L",");
 
 		REQUIRE(options.Parse(L"-file first, second double, third"));
 
-		CHECK(L"first, second double, third" == filename);
-
 		REQUIRE(3 == fileList.size());
-		CHECK(L"first" == fileList[0]);
-		CHECK(L"second double" == fileList[1]);
-		CHECK(L"third" == fileList[2]);
+		CHECK(fileList[0] == L"first");
+		CHECK(fileList[1] == L"second double");
+		CHECK(fileList[2] == L"third");
+	}
+
+	SECTION("Use custom value serial separator with a single-capture")
+	{
+		std::wstring filenames;
+
+		options.Add(L"file", L"", filenames);
+		options.SetSplitter(L",");
+
+		REQUIRE(options.Parse(L"-file first, second double, third"));
+
+		CHECK(filenames == L"first, second double, third");
+	}
+
+	SECTION("Use multiple character switch")
+	{
+		bool help;
+
+		options.Add(L"help", L"", help);
+		options.SetSwitch(L"--");
+
+		REQUIRE(options.Parse(L"--help"));
+
+		CHECK(help);
 	}
 
 	SECTION("Use multiple character key-value separator")
 	{
 		std::wstring filename;
+
+		options.Add(L"file", L"", filename);
+		options.SetSeparator(L"=>");
+
+		REQUIRE(options.Parse(L"-file=>filename"));
+
+		CHECK(filename == L"filename");
+	}
+
+	SECTION("Use multiple character value serial separator")
+	{
 		std::vector<std::wstring> fileList;
 
 		options.Add(L"file", L"", fileList);
-		options.Add(L"file", L"", filename);
-		options.SetSwitch(L"-[");
-		options.SetSeparator(L"]=");
-		options.SetSplitter(L".,");
+		options.SetSplitter(L";;");
 
-		REQUIRE(options.Parse(L"-[file]=first.,second double.,third"));
-
-		CHECK(L"first.,second double.,third" == filename);
+		REQUIRE(options.Parse(L"-file first;;second double;;third"));
 
 		REQUIRE(3 == fileList.size());
-		CHECK(L"first" == fileList[0]);
-		CHECK(L"second double" == fileList[1]);
-		CHECK(L"third" == fileList[2]);
+		CHECK(fileList[0] == L"first");
+		CHECK(fileList[1] == L"second double");
+		CHECK(fileList[2] == L"third");
+	}
+
+	SECTION("Use multiple character value serial separator with a single-capture")
+	{
+		std::wstring filenames;
+
+		options.Add(L"file", L"", filenames);
+		options.SetSplitter(L";;");
+
+		REQUIRE(options.Parse(L"-file first;;second double;;third"));
+
+		CHECK(filenames == L"first;;second double;;third");
+	}
+
+	SECTION("Use multiple character all-in-one")
+	{
+		std::vector<std::wstring> fileList;
+
+		options.Add(L"file", L"", fileList);
+		options.SetSwitch(L"-[");
+		options.SetSeparator(L"]=");
+		options.SetSplitter(L";;");
+
+		REQUIRE(options.Parse(L"-[file]=first;;second double;;third"));
+
+		REQUIRE(3 == fileList.size());
+		CHECK(fileList[0] == L"first");
+		CHECK(fileList[1] == L"second double");
+		CHECK(fileList[2] == L"third");
+	}
+
+	SECTION("Use multiple character all-in-one with a single-capture")
+	{
+		std::wstring filenames;
+
+		options.Add(L"file", L"", filenames);
+		options.SetSwitch(L"-[");
+		options.SetSeparator(L"]=");
+		options.SetSplitter(L";;");
+
+		REQUIRE(options.Parse(L"-[file]=first;;second double;;third"));
+
+		CHECK(filenames == L"first;;second double;;third");
 	}
 
 	SECTION("Use no-switch options")
 	{
-		std::vector<std::wstring> standaloneList;
 		std::vector<std::wstring> launchList;
 
-		options.Add(L"standalone", L"", standaloneList);
 		options.Add(L"launch", L"", launchList);
 		options.SetSwitch(L"");
 
 		REQUIRE(options.Parse(L"launch test localhost 109 1 1 1"));
 
-		CHECK(standaloneList.empty());
-
 		REQUIRE(6 == launchList.size());
-		CHECK(L"test" == launchList[0]);
-		CHECK(L"localhost" == launchList[1]);
-		CHECK(L"109" == launchList[2]);
-		CHECK(L"1" == launchList[3]);
-		CHECK(L"1" == launchList[4]);
-		CHECK(L"1" == launchList[5]);
+		CHECK(launchList[0] == L"test");
+		CHECK(launchList[1] == L"localhost");
+		CHECK(launchList[2] == L"109");
+		CHECK(launchList[3] == L"1");
+		CHECK(launchList[4] == L"1");
+		CHECK(launchList[5] == L"1");
+	}
+
+	SECTION("Use no-switch options not matching")
+	{
+		std::vector<std::wstring> noexitList;
+
+		options.Add(L"noexit", L"", noexitList);
+		options.SetSwitch(L"");
+
+		REQUIRE(options.Parse(L"launch test localhost 109 1 1 1"));
+
+		CHECK(noexitList.empty());
+	}
+
+	SECTION("Use no-switch options first argument not matching")
+	{
+		std::vector<std::wstring> localhostList;
+
+		options.Add(L"localhost", L"", localhostList);
+		options.SetSwitch(L"");
+
+		REQUIRE(options.Parse(L"launch test localhost 109 1 1 1"));
+
+		CHECK(localhostList.empty());
 	}
 
 	SECTION("Escape double quotes value")
 	{
-		std::wstring filename;
 		std::vector<std::wstring> fileList;
 
 		options.Add(L"file", L"", fileList);
-		options.Add(L"file", L"", filename);
 		options.SetSplitter(L",");
 
 		REQUIRE(options.Parse(L"-file \"first, second double, 3-rd\""));
 
-		CHECK(L"first, second double, 3-rd" == filename);
-
 		REQUIRE(3 == fileList.size());
-		CHECK(L"first" == fileList[0]);
-		CHECK(L"second double" == fileList[1]);
-		CHECK(L"3-rd" == fileList[2]);			// 스위치 포함 문자열
+		CHECK(fileList[0] == L"first");
+		CHECK(fileList[1] == L"second double");
+		CHECK(fileList[2] == L"3-rd"); // 스위치 포함 문자열
+	}
+
+	SECTION("Escape double quotes value with a single-capture")
+	{
+		std::wstring filenames;
+
+		options.Add(L"file", L"", filenames);
+		options.SetSplitter(L",");
+
+		REQUIRE(options.Parse(L"-file \"first, second double, 3-rd\""));
+
+		CHECK(filenames == L"first, second double, 3-rd");
 	}
 }
